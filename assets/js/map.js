ff2b99e3cbf37e88b233ff9382d3cc5477a95d80
richosj <okm760596@gmail.com>
2025-10-28 17:29:06 +0900

    Updates

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,4 @@
-import "../main.js";
+import "./main.js";
 console.log("Map main page JS loaded");
 document.addEventListener(`DOMContentLoaded`, () => {
   _0.els(`[data-samplescript]`).forEach((_target) => {
@@ -11,11 +11,17 @@ document.addEventListener(`DOMContentLoaded`, () => {
       if (_target.closest(`[data-groupWrap].slideTab`)) {
         _target.setAttribute(`data-pressed`, true);
       } else {
-        _target.setAttribute(`data-pressed`, _target.getAttribute(`data-pressed`) == `true` ? `false` : `true`);
+        _target.setAttribute(
+          `data-pressed`,
+          _target.getAttribute(`data-pressed`) == `true` ? `false` : `true`
+        );
       }
       if (_target.getAttribute(`data-group`)) {
-        _target.parentElement.closest(`[data-groupWrap`).querySelectorAll(`[data-group="${_target.getAttribute(`data-group`)}"]`).forEach((_groupItem) => {
-          if (_groupItem != _target) _groupItem.setAttribute(`data-pressed`, `false`);
+        _target.parentElement.closest(`[data-groupWrap`).querySelectorAll(
+          `[data-group="${_target.getAttribute(`data-group`)}"]`
+        ).forEach((_groupItem) => {
+          if (_groupItem != _target)
+            _groupItem.setAttribute(`data-pressed`, `false`);
         });
       }
     });
@@ -23,12 +29,20 @@ document.addEventListener(`DOMContentLoaded`, () => {
   _0.els(`input[type=radio][data-target]`).forEach((_tab) => {
     _tab.addEventListener(`click`, () => {
       _tab.closest(`[data-groupwrap]`).querySelectorAll(`input[name='${_tab.getAttribute(`name`)}']`).forEach((_siblings) => {
-        _0.el(`[data-title='${_siblings.getAttribute(`data-target`)}']`).setAttribute(`data-show`, `false`);
+        _0.el(
+          `[data-title='${_siblings.getAttribute(`data-target`)}']`
+        ).setAttribute(`data-show`, `false`);
       });
-      _0.el(`[data-title='${_tab.getAttribute(`data-target`)}']`).setAttribute(`data-show`, `true`);
+      _0.el(`[data-title='${_tab.getAttribute(`data-target`)}']`).setAttribute(
+        `data-show`,
+        `true`
+      );
     });
   });
-  _0.el(`section.list button.folding.show`).addEventListener(`click`, () => _0.result.show());
+  _0.el(`section.list button.folding.show`).addEventListener(
+    `click`,
+    () => _0.result.show()
+  );
   _0.el(`section.list button.folding.hide`).addEventListener(`click`, () => {
     _0.result.hide();
     _0.el(`nav.filter button[data-pressed='true']`).click();
@@ -37,8 +51,14 @@ document.addEventListener(`DOMContentLoaded`, () => {
     _button.addEventListener(`click`, () => {
       if (_button.getAttribute(`data-pressed`) == `true`) {
         _0.el(`.filterWrap`).setAttribute(`data-folded`, `false`);
-        _0.els(`.filterWrap .set`).forEach((_set) => _set.setAttribute(`data-show`, `false`));
-        _0.el(`.filterWrap .set[data-title="${_button.getAttribute(`data-target`)}"]`).setAttribute(`data-show`, _button.getAttribute(`data-pressed`));
+        _0.els(`.filterWrap .set`).forEach(
+          (_set) => _set.setAttribute(`data-show`, `false`)
+        );
+        _0.el(
+          `.filterWrap .set[data-title="${_button.getAttribute(
+            `data-target`
+          )}"]`
+        ).setAttribute(`data-show`, _button.getAttribute(`data-pressed`));
         _0.result.show();
       } else {
         _0.el(`.filterWrap`).setAttribute(`data-folded`, `true`);
@@ -48,9 +68,13 @@ document.addEventListener(`DOMContentLoaded`, () => {
   _0.els(`[data-selected]`).forEach((_target) => {
     _target.addEventListener(`click`, () => {
       _0.els(`[data-selectWrap] [data-selected]`).forEach((_siblings) => {
-        if (_siblings != _target) _siblings.setAttribute(`data-selected`, `false`);
+        if (_siblings != _target)
+          _siblings.setAttribute(`data-selected`, `false`);
       });
-      _target.setAttribute(`data-selected`, _target.getAttribute(`data-selected`) == `true` ? `false` : `true`);
+      _target.setAttribute(
+        `data-selected`,
+        _target.getAttribute(`data-selected`) == `true` ? `false` : `true`
+      );
       _target.getAttribute(`data-selected`) == `true` ? _0.detail.show() : _0.detail.hide();
     });
   });
@@ -59,15 +83,29 @@ document.addEventListener(`DOMContentLoaded`, () => {
   });
   _0.els(`aside.sidebar button[aria-haspopup="true"]`).forEach((_button) => {
     _button.addEventListener(`click`, () => {
-      _0.els(`aside.sidebar .popup`).forEach((_popup) => _popup.setAttribute(`data-show`, false));
-      _0.el(`aside.sidebar .popup[data-title="${_button.getAttribute(`data-target`)}"]`).setAttribute(`data-show`, _button.getAttribute(`data-pressed`));
+      _0.els(`aside.sidebar .popup`).forEach(
+        (_popup) => _popup.setAttribute(`data-show`, false)
+      );
+      _0.el(
+        `aside.sidebar .popup[data-title="${_button.getAttribute(
+          `data-target`
+        )}"]`
+      ).setAttribute(`data-show`, _button.getAttribute(`data-pressed`));
     });
   });
   _0.els(`aside.sidebar .popup button.close`).forEach((_button) => {
-    _button.addEventListener(`click`, () => _0.el(`aside.sidebar button.menu[data-target='${_button.closest(`.popup`).getAttribute(`data-title`)}']`).click());
+    _button.addEventListener(
+      `click`,
+      () => _0.el(
+        `aside.sidebar button.menu[data-target='${_button.closest(`.popup`).getAttribute(`data-title`)}']`
+      ).click()
+    );
   });
   _0.els(`.modal header button.close`).forEach((_button) => {
-    _button.addEventListener(`click`, () => _button.closest(`.modal`).setAttribute(`data-show`, `false`));
+    _button.addEventListener(
+      `click`,
+      () => _button.closest(`.modal`).setAttribute(`data-show`, `false`)
+    );
   });
 });
 const _0 = {
@@ -95,9 +133,16 @@ const _0 = {
   },
   filter: {
     reset: function() {
-      _0.els(`nav.filter button`).forEach((_button) => _button.disabled = false);
-      _0.el(`nav.filter button[data-pressed='true']`).classList.remove(`applyed`);
-      _0.el(`nav.filter button[data-pressed='true']`).setAttribute(`data-pressed`, `false`);
+      _0.els(`nav.filter button`).forEach(
+        (_button) => _button.disabled = false
+      );
+      _0.el(`nav.filter button[data-pressed='true']`).classList.remove(
+        `applyed`
+      );
+      _0.el(`nav.filter button[data-pressed='true']`).setAttribute(
+        `data-pressed`,
+        `false`
+      );
       _0.el(`.filterWrap`).setAttribute(`data-folded`, `true`);
       _0.el(`.list`).setAttribute(`data-state`, null);
     }
@@ -105,9 +150,13 @@ const _0 = {
   sample: {
     filterApply: function() {
       _0.el(`.list`).setAttribute(`data-state`, `filterApplyed`);
-      _0.els(`nav.filter button`).forEach((_button) => _button.disabled = true);
+      _0.els(`nav.filter button`).forEach(
+        (_button) => _button.disabled = true
+      );
       _0.el(`nav.filter button[data-pressed='true']`).classList.add(`applyed`);
-      _0.el(`nav.filter button[data-pressed='true'] span`).textContent = `선택사항`;
+      _0.el(
+        `nav.filter button[data-pressed='true'] span`
+      ).textContent = `선택사항`;
     },
     filterReset: function() {
       _0.filter.reset();
